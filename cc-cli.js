14d0c439891287352fb4a363943c8d78bac16f20
Casey Cann <casey@Caseys-iMac.local>
2020-06-24 09:14:27 -0400

    Reformatting repo

--- cc-cli.js
+++ cc-cli.js
@@ -4,5 +4,5 @@ var moment = require('moment');
 
 var theDate = moment().format('YYYYMMDD-HH:mm:ss.SSS');
 
-console.log(`cli.js works!`);
+console.log(`cc-cli.js works!`);
 console.log(`the date and time are ${theDate}`);
